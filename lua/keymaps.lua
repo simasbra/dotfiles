3d0aa9359bdfa9a351e5d45eb2a07395996cc5f4
Simas Bradaitis <simasbra@proton.me>
2024-11-01 17:01:46 +0200

    keymap refactor

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -13,9 +13,9 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz", { noremap = true, desc = "Move [u]p a pa
 
 -- Colemak remappings
 vim.keymap.set("n", "<C-w>h", "<C-w><C-h>", { noremap = true, desc = "Go to the left window" })
-vim.keymap.set("n", "<C-w>e", "<C-w><C-l>", { noremap = true, desc = "Go to the right window" })
 vim.keymap.set("n", "<C-w>n", "<C-w><C-j>", { noremap = true, desc = "Go to the down window" })
 vim.keymap.set("n", "<C-w>l", "<C-w><C-k>", { noremap = true, desc = "Go to the up window" })
+vim.keymap.set("n", "<C-w>e", "<C-w><C-l>", { noremap = true, desc = "Go to the right window" })
 
 local map = vim.keymap.set
 local remappings = {
@@ -28,23 +28,17 @@ local remappings = {
 	{ "n", "e", "l", "Right (swap l)" },
 	{ "n", "K", "Nzz", "Previous find (swap N)" }, -- Also centers the text
 	{ "n", "k", "nzz", "Next find (swap n)" }, -- Also centers the text
-	{ "n", "H", "H", "Screen top" },
-	{ "n", "h", "h", "Left" },
-	{ "n", "L", "L", "Screen bottom" },
 	{ "n", "l", "k", "Up (swap k)" },
 	-- Visual mode remappings
-	{ "v", "J", "E", "Jump end WORD (swap E)" },
-	{ "v", "j", "e", "Jump end word (swap e)" },
-	{ "v", "N", "K", "Help (swap K)" },
-	{ "v", "n", "j", "Down (swap j)" },
-	{ "v", "E", "J", "Join lines (swap J)" },
-	{ "v", "e", "l", "Right (swap l)" },
-	{ "v", "K", "Nzz", "Previous find (swap N)" }, -- Also centers the text
-	{ "v", "k", "nzz", "Next find (swap n)" }, -- Also centers the text
-	{ "v", "H", "H", "Screen top" },
-	{ "v", "h", "h", "Left" },
-	{ "v", "L", "L", "Screen bottom" },
-	{ "v", "l", "k", "Up (swap k)" },
+	{ "v", "J", "E", "" },
+	{ "v", "j", "e", "" },
+	{ "v", "N", "K", "" },
+	{ "v", "n", "j", "" },
+	{ "v", "E", "J", "" },
+	{ "v", "e", "l", "" },
+	{ "v", "K", "Nzz", "" }, -- Also centers the text
+	{ "v", "k", "nzz", "" }, -- Also centers the text
+	{ "v", "l", "k", "" },
 }
 -- Apply the remappings
 for _, mapping in ipairs(remappings) do
