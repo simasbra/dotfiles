bf2c5211de6bcdbd8056e48fa307823d247e04b2
Simas Bradaitis <simasbra@proton.me>
2024-11-02 23:15:51 +0200

    gitsigns hunk

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -33,7 +33,7 @@ return {
 			current_line_blame_opts = {
 				virt_text = true,
 				virt_text_pos = "eol", -- "eol" | "overlay" | "right_align"
-				delay = 1000,
+				delay = 3000,
 				ignore_whitespace = false,
 				virt_text_priority = 100,
 				use_focus = true,
@@ -78,22 +78,22 @@ return {
 				end, { desc = "previous hunk" })
 
 				-- Actions
-				map('n', '<leader>hs', gitsigns.stage_hunk, { desc = "[H]unk [s]tage" })
-				map('n', '<leader>hr', gitsigns.reset_hunk, { desc = "[H]unk [r]eset" })
-				map('v', '<leader>hs', function() gitsigns.stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end, { desc = "[H]unk [s]tage" })
-				map('v', '<leader>hr', function() gitsigns.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end, { desc = "[H]unk [r]eset" })
-				map('n', '<leader>hS', gitsigns.stage_buffer, { desc = "[H]unk [S]tage buffer" })
-				map('n', '<leader>hu', gitsigns.undo_stage_hunk, { desc = "[H]unk [u]undo stage" })
-				map('n', '<leader>hR', gitsigns.reset_buffer, { desc = "[H]unk [R]eset buffer" })
-				map('n', '<leader>hp', gitsigns.preview_hunk, { desc = "[H]unk [p]review" })
-				map('n', '<leader>tB', function() gitsigns.blame_line{full=true} end, { desc = "[t]oggle [B]lame" })
-				map('n', '<leader>tb', gitsigns.toggle_current_line_blame, { desc = "[t]oggle current line [b]lame" })
+				map('n', '<leader>hs', gitsigns.stage_hunk, { desc = "[h]unk [s]tage" })
+				map('n', '<leader>hr', gitsigns.reset_hunk, { desc = "[h]unk [r]eset" })
+				map('v', '<leader>hs', function() gitsigns.stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end, { desc = "[h]unk [s]tage" })
+				map('v', '<leader>hr', function() gitsigns.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end, { desc = "[h]unk [r]eset" })
+				map('n', '<leader>hS', gitsigns.stage_buffer, { desc = "[h]unk [S]tage buffer" })
+				map('n', '<leader>hu', gitsigns.undo_stage_hunk, { desc = "[h]unk [u]undo stage" })
+				map('n', '<leader>hR', gitsigns.reset_buffer, { desc = "[h]unk [R]eset buffer" })
+				map('n', '<leader>hp', gitsigns.preview_hunk, { desc = "[h]unk [p]review" })
+				map('n', '<leader>tb', function() gitsigns.blame_line{full=true} end, { desc = "[t]oggle [b]lame" })
+				map('n', '<leader>tB', gitsigns.toggle_current_line_blame, { desc = "[t]oggle current line [B]lame preview" })
 				map('n', '<leader>td', gitsigns.diffthis, { desc = "[t]oggle [d]ifference" })
 				map('n', '<leader>tD', function() gitsigns.diffthis('~') end, { desc = "[t]oggle [D]ifference ~" })
 				map('n', '<leader>td', gitsigns.toggle_deleted, { desc = "[t]oggle [d]eleted" })
 
 				-- Text object
-				map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+				-- map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
 			end
 		}
 	end
