ffb0af93d2f81db613ef2fe35fe8dcd67963ce8b
Simas Bradaitis <simasbra@proton.me>
2025-05-06 23:03:20 +0300

    sway config

--- sway/status_bar.sh
+++ sway/status_bar.sh
@@ -4,6 +4,12 @@ while true; do
 	DATE=$(date +'%c')
 	LAYOUT=$(swaymsg -t get_inputs |
 		jq -r 'map(select(has("xkb_active_layout_name")))[0].xkb_active_layout_name')
-	echo "$LAYOUT | $DATE"
+	if [ $(playerctl status) = "Playing" ]; then
+		ARTIST=$(playerctl metadata artist)
+		SONG=$(playerctl metadata title)
+		echo "$ARTIST - $SONG | $LAYOUT | $DATE"
+	else
+		echo "$LAYOUT | $DATE"
+	fi
 	sleep 1
 done
