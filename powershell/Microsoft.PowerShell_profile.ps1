26735f1592d478d8f2bec6c1800c8c851dad0772
Simas Bradaitis <simasbra@proton.me>
2025-06-13 09:49:23 +0300

    fucking powershel atleast completing normally

---
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,6 @@
+Set-Alias -Name "rm-rf" -Value Remove-Item -Option AllScope -Force
+
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
+Set-PSReadLineOption -HistorySearchCursorMovesToEnd
