b2beab548465e81afbc29699a35d960b5effeefd
Simas Bradaitis <simas.bradaitis@lsmu.lt>
2025-07-28 09:19:33 +0300

    conform exception for windows

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -72,12 +72,12 @@ return {
 				pyink = {
 					prepend_args = { "--line-length", "100" },
 				},
-				["clang-format"] = {
+				 ["clang-format"] = vim.fn.has("win32") == 0 and {
 					prepend_args = {
 						"-style=file:"
 							.. os.getenv("XDG_CONFIG_HOME")
 							.. "/clang-format/config.yaml",
-					},
+					} or {},
 				},
 				prettier = {
 					prepend_args = { "--print-width", "100" },
