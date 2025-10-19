3b811816cc03a738f40b58bfc21a681daf4e7fcb
Simas Bradaitis <simasbra@proton.me>
2025-10-20 00:11:58 +0300

    sway

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -19,9 +19,9 @@ return {
 			-- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
 			--  If you are experiencing weird indenting issues, add the language to
 			--  the list of additional_vim_regex_highlighting and disabled languages for indent.
-			additional_vim_regex_highlighting = { "ruby", "csharp" },
+			additional_vim_regex_highlighting = { "ruby" },
 		},
-		indent = { enable = true, disable = { "ruby", "python", "csharp" } },
+		indent = { enable = true, disable = { "ruby", "python" } },
 	},
 	config = function(_, opts)
 		-- See `:help nvim-treesitter`
