04f9380c405187e7434b4f46a146831a0916b9b0
Simas Bradaitis <simasbra@proton.me>
2025-10-22 09:02:09 +0300

    waybar

--- sway/scripts/status_bar.sh
+++ sway/scripts/status_bar.sh
@@ -6,20 +6,22 @@ while true; do
 	DATE=$(date +'%c')
 	LAYOUT=$(swaymsg -t get_inputs |
 		jq -r 'map(select(has("xkb_active_layout_name")))[0].xkb_active_layout_name')
-	PERCENTAGE=$(cat /sys/class/power_supply/macsmc-battery/capacity)
+	BATTERY_PERCENTAGE=$(cat /sys/class/power_supply/macsmc-battery/capacity)
+	BATTERY_STATUS=$(cat /sys/class/power_supply/macsmc-battery/status)
+	BATTERY="$BATTERY_STATUS $BATTERY_PERCENTAGE%"
 
 	if [ $(playerctl status) = "Playing" ]; then
 		ARTIST=$(playerctl metadata artist)
 		SONG=$(playerctl metadata title)
 
 		if [ $CHASSIS = "laptop" ]; then
-			echo "$ARTIST - $SONG | $PERCENTAGE% | $LAYOUT | $DATE"
+			echo "$ARTIST - $SONG | $BATTERY | $LAYOUT | $DATE"
 		else
 			echo "$ARTIST - $SONG | $LAYOUT | $DATE"
 		fi
 	else
 		if [ $CHASSIS = "laptop" ]; then
-			echo "$LAYOUT | $PERCENTAGE% | $DATE"
+			echo "$LAYOUT | $BATTERY | $DATE"
 		else
 			echo "$LAYOUT | $DATE"
 		fi
