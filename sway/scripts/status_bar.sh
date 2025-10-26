16f47f300437474d804b40ae94ad60af78f43129
Simas Bradaitis <simasbra@proton.me>
2025-10-26 23:38:14 +0200

    sway waybar and vlc configs

--- sway/scripts/status_bar.sh
+++ sway/scripts/status_bar.sh
@@ -3,28 +3,21 @@
 CHASSIS=$(hostnamectl chassis)
 
 while true; do
-	DATE=$(date +'%c')
-	LAYOUT=$(swaymsg -t get_inputs |
-		jq -r 'map(select(has("xkb_active_layout_name")))[0].xkb_active_layout_name')
-	BATTERY_PERCENTAGE=$(cat /sys/class/power_supply/macsmc-battery/capacity)
-	BATTERY_STATUS=$(cat /sys/class/power_supply/macsmc-battery/status)
-	BATTERY="$BATTERY_STATUS $BATTERY_PERCENTAGE%"
+	DATE=$(date +'%b %d, %H:%M')
+	LAYOUT=$(swaymsg -t get_inputs | jq -r '.[0].xkb_active_layout_name')
+
+	OUTPUT="$LAYOUT | $DATE"
+
+	if [ $CHASSIS = "laptop" ]; then
+		BATTERY=$(cat /sys/class/power_supply/macsmc-battery/capacity)
+		OUTPUT="$BATTERY | $OUTPUT"
+	fi
 
 	if [ $(playerctl status) = "Playing" ]; then
 		ARTIST=$(playerctl metadata artist)
 		SONG=$(playerctl metadata title)
-
-		if [ $CHASSIS = "laptop" ]; then
-			echo "$ARTIST - $SONG | $BATTERY | $LAYOUT | $DATE"
-		else
-			echo "$ARTIST - $SONG | $LAYOUT | $DATE"
-		fi
-	else
-		if [ $CHASSIS = "laptop" ]; then
-			echo "$LAYOUT | $BATTERY | $DATE"
-		else
-			echo "$LAYOUT | $DATE"
-		fi
+		echo "$ARTIST - $SONG | $OUTPUT"
 	fi
+
 	sleep 1
 done
