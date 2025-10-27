22e2a78dbaff32532d33bb3ea523465dce40f4f1
Simas Bradaitis <simasbra@proton.me>
2025-10-27 20:31:09 +0200

    wayland

--- sway/scripts/status_bar.sh
+++ sway/scripts/status_bar.sh
@@ -4,20 +4,21 @@ CHASSIS=$(hostnamectl chassis)
 
 while true; do
 	DATE=$(date +'%b %d, %H:%M')
-	LAYOUT=$(swaymsg -t get_inputs | jq -r '.[0].xkb_active_layout_name')
+	LAYOUT=$(swaymsg -t get_inputs | jq -r 'map(select(has("xkb_active_layout_name")))[0].xkb_active_layout_name')
 
 	OUTPUT="$LAYOUT | $DATE"
 
 	if [ $CHASSIS = "laptop" ]; then
 		BATTERY=$(cat /sys/class/power_supply/macsmc-battery/capacity)
-		OUTPUT="$BATTERY | $OUTPUT"
+		OUTPUT="$BATTERY% | $OUTPUT"
 	fi
 
 	if [ $(playerctl status) = "Playing" ]; then
 		ARTIST=$(playerctl metadata artist)
 		SONG=$(playerctl metadata title)
-		echo "$ARTIST - $SONG | $OUTPUT"
+		OUTPUT="$ARTIST - $SONG | $OUTPUT"
 	fi
+	echo $OUTPUT
 
 	sleep 1
 done
