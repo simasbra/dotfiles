26210ee353f1812aa4e86de62987aafcfabe94da
Simas Bradaitis <simasbra@proton.me>
2025-11-04 23:08:48 +0200

    keyboard layout

--- sway/scripts/keyboard_notifications.sh
+++ sway/scripts/keyboard_notifications.sh
@@ -3,12 +3,14 @@
 set -e
 
 if pidof -o %PPID -x "keyboard_notifications.sh" >/dev/null; then
+	echo "yes"
 	exit
 fi
 
 swaymsg -mt subscribe '["input"]' | while read -r INPUT; do
 	IS_LAYOUT_CHANGE=$(echo $INPUT | jq -r '.change == "xkb_layout"')
-	if [[ "$IS_LAYOUT_CHANGE" == "true" ]]; then
+	IS_KEYBOARD=$(echo $INPUT | jq -r '.input.type == "keyboard"')
+	if [[ "$IS_LAYOUT_CHANGE" == "true" ]] && [[ "$IS_KEYBOARD" == "true" ]]; then
 		LAYOUT=$(echo $INPUT | jq -r '.input.xkb_active_layout_name')
 		notify-send --expire-time 3000 "Keyboard layout changed" "$LAYOUT"
 	fi
